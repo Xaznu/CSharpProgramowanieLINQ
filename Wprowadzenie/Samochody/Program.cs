027517c66cadd649e8cb32a980ed1909703239c3
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-27 19:35:51 +0200

    Implementacja wczytywania pliku

--- Wprowadzenie/Samochody/Program.cs
+++ Wprowadzenie/Samochody/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Samochody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var samochody = WczytywaniePliku("paliwo.csv");
+
+            foreach (var samochod in samochody)
+            {
+                Console.WriteLine(samochod.Producent + " " + samochod.Model);
+            }
+        }
+
+        private static List<Samochod> WczytywaniePliku(string sciezka)
+        {
+            return File.ReadAllLines(sciezka)
+                       .Skip(1)
+                       .Where(linia => linia.Length > 1)
+                       .Select(Samochod.ParsujCSV).ToList();
+        }
+    }
+}
--- Wprowadzenie/Samochody/Samochod.cs
+++ Wprowadzenie/Samochody/Samochod.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Samochody
 {
-    internal class Samochod
+    public class Samochod
     {
+        public int Rok { get; set; }
+        public string Producent { get; set; }
+        public string Model { get; set; }
+        public double Pojemnosc { get; set; }
+        public int IloscCylindrow { get; set; }
+        public int SpalanieMiasto { get; set; }
+        public int SpalanieAutostrada { get; set; }
+        public int SpalanieMieszane { get; set; }
+
+        internal static Samochod ParsujCSV(string linia)
+        {
+            var kolumny = linia.Split(',');
+
+            return new Samochod
+            {
+                Rok = int.Parse(kolumny[0]),
+                Producent = kolumny[1],
+                Model = kolumny[2],
+                Pojemnosc = double.Parse(kolumny[3]),
+                IloscCylindrow = int.Parse(kolumny[4]),
+                SpalanieMiasto = int.Parse(kolumny[5]),
+                SpalanieAutostrada = int.Parse(kolumny[6]),
+                SpalanieMieszane = int.Parse(kolumny[7])
+            };
+        }
     }
 }
